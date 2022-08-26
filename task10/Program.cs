308b76ef9abb57bdaa1828e73a6f45e7893ae175
Dariya Popova <dariyp26@mail.ru>
2022-08-26 10:04:26 +0300

    ДЗ к семинару 2

---
+++ task10/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int number = new Random().Next(100,1000);
+Console.WriteLine();
+Console.WriteLine("Случайное трёхзначное число: " + number + ".");
+number = number / 10;
+number = number % 10;
+Console.WriteLine("Вторая цифра в этом числе: " + number + ".");
+Console.WriteLine();
---
+++ task15/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+
+int day = new Random().Next(1,8);
+Console.WriteLine("День недели: " + day + ".");
+
+// Вариант с оператором IF-ELSE
+/*if (day < 6)  Console.WriteLine("Это рабочий день.");
+else Console.WriteLine("Это ВЫХОДНОЙ.");*/
+
+// Вариант с оператором SWITCH
+switch (day)
+{
+  case < 6:
+    Console.WriteLine("Это рабочий день.");
+    break;
+  default:
+    Console.WriteLine("Это ВЫХОДНОЙ.");
+    break;
+}
